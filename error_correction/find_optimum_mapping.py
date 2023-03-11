d6d23810741c6d03d5444ef23be4496d28569370
Golam Mortuza <golammdmortuza@u.boisestate.edu>
2023-03-10 19:59:55 -0800

    cell distribution done; checksum mapping done

--- error_correction/find_optimum_mapping.py
+++ error_correction/find_optimum_mapping.py
@@ -40,9 +40,9 @@ class Mapping:
 
         self.total_orientation_bits = 4
         self.total_bits = row * column * layer
-        self.total_parity_bits = int(self.total_bits * parity_percent)
-        self.total_check_sum_bits = int(self.total_bits * check_sum_percent)
-        self.total_data_bits = self.total_bits - self.total_parity_bits - self.total_check_sum_bits - \
+        self.total_parity_cells = int(self.total_bits * parity_percent)
+        self.total_checksum_cells = int(self.total_bits * check_sum_percent)
+        self.total_data_bits = self.total_bits - self.total_parity_cells - self.total_checksum_cells - \
                                self.total_index_bits - self.total_orientation_bits
         if self.total_data_bits <= 0:
             raise Exception("Invalid settings. Total data bits can not be negative. Reduce the parity percent or "
@@ -86,27 +86,6 @@ class Mapping:
         for axis in ['x', 'y', 'xy']:
             mirrored_points.extend(self.get_mirror_point([point], axis))
         return mirrored_points
-
-    # The bits that are assigned for parity
-    def get_parity_bits(self) -> [tuple]:
-        # TODO: fix this
-        # parity bits is only on the second layer
-        parity_bits = []
-        for row in range(self.row):
-            for column in range(self.column):
-                parity_bits.append((LAYER_ASSIGNED_TO_PARITY_BITS, row, column))
-        return parity_bits
-
-    # The bits that are assigned for data, indexing and orientation
-    def get_data_bits(self):
-        # TODO: fix this
-        data_bits = []
-        layers_assigned_to_data_bits = set(range(self.layer)) - set([LAYER_ASSIGNED_TO_PARITY_BITS])
-        for layer in layers_assigned_to_data_bits:
-            for row in range(self.row):
-                for column in range(self.column):
-                    data_bits.append((layer, row, column))
-        return data_bits
 
     # we can not use the same data bits for a parity bit that is mirrored to the other parity bits.
     # This function will return the list of data bits that are not mirrored to the other parity bits
@@ -152,19 +131,16 @@ class Mapping:
             data_bit_counter[data_bit] += 1  # replace inplace
 
     # Generate the mapping for parity bits
-    def get_mapping(self) -> dict:
+    def get_parity_mapping(self, parity_bits, rest_bits) -> dict:
+        # TODO: double check the mapping
         # track number of parity that is assigned to each data bit
         data_bit_counter = defaultdict(lambda: 0)
-        data_bit_counter = defaultdict(int)
         parity_bit_counter = defaultdict(int)
-        check_sum_bit_counter = defaultdict(int)
-        parity_bits = self.get_parity_bits()
-        data_bits = self.get_data_bits()
         mapping = {}
         # assign random PARITY_COVERAGE data bits to that parity bits
         for parity_bit in parity_bits:  # get data bits for all parity bits
             if parity_bit not in mapping:
-                available_data_bits = self.get_available_data_bits_for_a_parity_bit(parity_bit, data_bits, mapping)
+                available_data_bits = self.get_available_data_bits_for_a_parity_bit(parity_bit, rest_bits, mapping)
                 assigned_data_bits = self.get_assigned_points_for_parity(data_bit_counter, available_data_bits)
                 mapping[parity_bit] = assigned_data_bits
                 self.update_counter(data_bit_counter, assigned_data_bits)
@@ -175,6 +151,35 @@ class Mapping:
                     mapping[self.get_mirror_point([parity_bit], axis)[0]] = assigned_data_bits_mirrored
         return mapping
 
+    def get_checksum_mapping(self, checksum_cells, rest_cells) -> dict:
+        # each of the checksum cell will have cell assigned to it
+        # cell and checksum will have same levels
+        # each cell except for parity cell must be assigned to a checksum cell
+        # rest_cells by levels
+        rest_cells_by_level = defaultdict(set)
+        for cell in rest_cells:
+            rest_cells_by_level[cell[0]].add(cell)
+        checksum_mapping = defaultdict(list)
+        while len(rest_cells_by_level) > 0:
+            # don't choose random checksum cell, choose the one with the least number of assigned cells
+            checksum_cell = sorted(checksum_cells, key=lambda x: len(checksum_mapping[x]), reverse=False)[0]
+            # assign a random cell to the checksum cell
+            assigned_cell = random.choice(list(rest_cells_by_level[checksum_cell[0]]))
+            checksum_mapping[checksum_cell].append(assigned_cell)
+            # remove the assigned cell from the rest cells
+            rest_cells_by_level[checksum_cell[0]].remove(assigned_cell)
+            # add their mirror points to the checksum mapping
+            for axis in ['x', 'y', 'xy']:
+                check_sum_mirror_point = self.get_mirror_point([checksum_cell], axis)[0]
+                assigned_cell_mirror_point = self.get_mirror_point([assigned_cell], axis)[0]
+                checksum_mapping[check_sum_mirror_point].append(assigned_cell_mirror_point)
+                # remove the assigned cell from the rest cells
+                rest_cells_by_level[checksum_cell[0]].remove(assigned_cell_mirror_point)
+            if len(rest_cells_by_level[checksum_cell[0]]) == 0:
+                del rest_cells_by_level[checksum_cell[0]]
+
+        return checksum_mapping
+
     # get distance between parity bit and the assigned data bit
     def get_distance_point_of_mapping(self, mapping):
         distances = {}
@@ -182,7 +187,7 @@ class Mapping:
             distances[parity_bit] = self.get_distance_multiple_point(parity_bit, data_bits)
         return distances
 
-    def get_all_bits(self):
+    def get_all_cells(self):
         all_bits = []
         for layer in range(self.layer):
             for row in range(self.row):
@@ -190,42 +195,71 @@ class Mapping:
                     all_bits.append((layer, row, column))
         return all_bits
 
+    def get_random_cells_evenly_distributed_by_levels(self, available_cells, total_cells):
+        available_cells_by_layer = defaultdict(set)
+        for cell in available_cells:
+            available_cells_by_layer[cell[0]].add(cell)
+        # each level will have same amount of parity bits
+        number_of_cells_per_level = total_cells // self.layer
+        # draw a random cell from each level and assign it as a parity bit
+        random_cells = []
+        while len(random_cells) < total_cells:
+            # randomize which layer to choose from
+            randomized_layer = list(range(self.layer))
+            random.shuffle(randomized_layer)
+            for layer in randomized_layer:
+                if len(random_cells) >= total_cells:
+                    break
+                random_cell = random.choice(list(available_cells_by_layer[layer]))
+                # get the mirror point of this random cell
+                mirror_point = self.get_mirror_points_all_axis(random_cell)
+                # remove the mirror point from the available bits
+                available_cells_by_layer[layer].difference_update(set(mirror_point))
+                random_cells.extend(mirror_point)
+        return random_cells
+
     # Set the bits purpose(parity, checksum, data, orientation, indexing)
-    def determine_bits(self):
+    def determine_cells_purpose(self):
         # all bits
-        available_bits = self.get_all_bits()
+        available_cells = self.get_all_cells()
+        # available_cells = self.get_all_bits()
         # first determine the orientation bit
-        # for now put four orientation bits
+        # for now put four orientation bits hardcoded
         # first layer four corners
-        orientation_bits = [(0, 0, 0), (0, 0, self.column - 1), (0, self.row - 1, 0),
-                            (0, self.row - 1, self.column - 1)]
+        orientation_cells = [(0, 0, 0), (0, 0, self.column - 1), (0, self.row - 1, 0),
+                             (0, self.row - 1, self.column - 1)]
         # remove the orientation bits from the available bits
-        available_bits = list(set(available_bits).difference(set(orientation_bits)))
+        available_cells = list(set(available_cells).difference(set(orientation_cells)))
         # get the parity bits
-        parity_bits = random.sample(available_bits, self.total_parity_bits)
+        parity_cells = self.get_random_cells_evenly_distributed_by_levels(available_cells, self.total_parity_cells)
         # remove the parity bits from the available bits
-        available_bits = list(set(available_bits).difference(set(parity_bits)))
+        available_cells = list(set(available_cells).difference(set(parity_cells)))
         # get the checksum bits
-        checksum_bits = random.sample(available_bits, self.total_check_sum_bits)
+        checksum_cells = self.get_random_cells_evenly_distributed_by_levels(available_cells, self.total_checksum_cells)
         # remove the checksum bits from the available bits
-        available_bits = list(set(available_bits).difference(set(checksum_bits)))
+        available_cells = list(set(available_cells).difference(set(checksum_cells)))
         # get the indexing bits
-        indexing_bits = random.sample(available_bits, self.total_index_bits)
+        indexing_cells = self.get_random_cells_evenly_distributed_by_levels(available_cells, self.total_index_bits)
         # indexing, orientation, data will follow a particular order
-        indexing_bits.sort()
+        indexing_cells.sort()
         # remove the indexing bits from the available bits
-        available_bits = list(set(available_bits).difference(set(indexing_bits)))
+        available_cells = list(set(available_cells).difference(set(indexing_cells)))
         # get the data bits
-        data_bits = available_bits
-        data_bits.sort()
-        bits_role = {
-            'parity_bits': parity_bits,
-            'checksum_bits': checksum_bits,
-            'indexing_bits': indexing_bits,
-            'data_bits': data_bits,
-            'orientation_bits': orientation_bits
+        data_cells = available_cells
+        data_cells.sort()
+        return {
+            'parity_bits': parity_cells,
+            'checksum_cells': checksum_cells,
+            'indexing_bits': indexing_cells,
+            'data_bits': data_cells,
+            'orientation_bits': orientation_cells,
+            'all_but_parity': checksum_cells + indexing_cells + data_cells + orientation_cells,
+            'all_but_parity_checksum': indexing_cells + data_cells + orientation_cells,
         }
-        return bits_role
+
+    def get_parity_checksum_cells(self, available_cells, total):
+        bits = []
+        # while total <
 
     # Get a point for the mapping based on the distance of the parity bit and the assigned data bit
     def get_mapping_point(self, mapping):
@@ -236,23 +270,28 @@ class Mapping:
         return mapping_point / len(mapping.keys())
 
     def get_optimum_mapping(self):
+        # define the purpose of each bit(parity, checksum, data, orientation, indexing)
         highest_point = float('-inf')
         for _ in range(NUMBER_OF_RUN):
-            mapping = self.get_mapping()
-            mapping_point = self.get_mapping_point(mapping)
-            if mapping_point > highest_point:
-                highest_point = mapping_point
-                best_mapping = mapping
-        mapper_details = {
-            'parity_mapper': None,
-            'checksum_mapper': None,
-            'parity_bits': None,
-            'checksum_bits': None,
-            'data_bits': None,
-            'orientation_bit': None,
-            'index_bit': None,
-        }
-        return mapper_details
+            cell_purpose = self.determine_cells_purpose()
+            # get parity mapper
+            current_parity_mapping = self.get_parity_mapping(cell_purpose['parity_bits'],
+                                                             cell_purpose['all_but_parity'])
+            # get checksum mapping
+            current_checksum_mapping = self.get_checksum_mapping(cell_purpose['checksum_cells'],
+                                                                 cell_purpose['all_but_parity_checksum'])
+
+            current_parity_mapping_point = self.get_mapping_point(current_parity_mapping)
+            current_checksum_mapping_point = self.get_mapping_point(current_checksum_mapping)
+            total_mapping_point = current_parity_mapping_point + current_checksum_mapping_point
+
+            if total_mapping_point > highest_point:
+                highest_point = total_mapping_point
+                best_checksum_mapping = current_checksum_mapping
+                best_parity_mapping = current_parity_mapping
+                best_bits_purpose = bits_purpose
+        # combine the best mapping and the best bit purpose
+        return None
 
     def test_get_mirror_point(self):
         points = [(0, 0, 0), (1, 1, 1), (2, 2, 2)]
@@ -269,5 +308,5 @@ class Mapping:
 
 
 if __name__ == '__main__':
-    mapping = Mapping()
-    mapping.get_optimum_mapping()
+    mapping_ = Mapping()
+    mapping_.get_optimum_mapping()
